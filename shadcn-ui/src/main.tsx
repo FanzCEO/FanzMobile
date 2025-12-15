535140392e75c8252e7b063c28695958a5fc193d
Joshua Stone <jstonecna@gmail.com>
2025-12-15 11:58:15 -0500

    commit

--- shadcn-ui/src/main.tsx
+++ shadcn-ui/src/main.tsx
@@ -1,5 +1,11 @@
 import { createRoot } from 'react-dom/client';
+import { SplashScreen } from '@capacitor/splash-screen';
 import App from './App.tsx';
 import './index.css';
+
+// Hide splash screen when app is ready
+SplashScreen.hide().catch(() => {
+  // Ignore errors on web - SplashScreen only works on native
+});
 
 createRoot(document.getElementById('root')!).render(<App />);
